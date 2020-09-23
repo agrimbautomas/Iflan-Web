592b097fdb03803c5fc4b0efc2d64e186965c7bc
Tomas Agrimbau <tomas@amalgama.co>
2020-09-22 22:52:39 -0300

    Update App component

--- src/index.js
+++ src/index.js
@@ -2,30 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import './index.scss';
 import * as serviceWorker from './serviceWorker'
-import Header from "./components/Header/Header";
-import HomeBlocksContainer from "./components/HomeBlocksContainer/HomeBlocksContainer";
-import InstallButton from "./components/InstallButton/InstallButton";
+import App from "./App";
 
-
-class MainGrid extends React.Component {
-  render() {
-	return (
-	  <div>
-		<Header/>
-		<HomeBlocksContainer/>
-		<InstallButton/>
-	  </div>
-	)
-  }
-}
 
 ReactDOM.render(
-  <MainGrid/>,
+  <App/>,
   document.getElementById('root')
 );
 
 serviceWorker.register();
-
-
-
-

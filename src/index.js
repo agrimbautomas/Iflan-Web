35847b80a156db1022d7ff58d7829ea0dab261ca
Tomas Agrimbau <tomas@amalgama.co>
2020-09-23 01:32:47 -0300

    Add redux

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import * as serviceWorker from './serviceWorker'
 import App from "./App";
+import { store } from './store/store';
+import { Provider } from "react-redux";
 
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
+  <Provider store={ store }>
+	<App/>
+  </Provider>,
+  rootElement
 );
+
 
 serviceWorker.register();
